6c58684be50c772098a70373d35a4ae16b43211b
Brendan <bjregnery@ucdavis.edu>
2023-08-15 20:37:48 +0200

    QCD only

--- training.py
+++ training.py
@@ -186,8 +186,8 @@ def main():
     signal_cols = [Columns.load(f) for f in glob.glob(DATADIR+'/train_signal/*.npz')]
     bkg_cols = [
         Columns.load(f) for f in
-        glob.glob(DATADIR+'/train_bkg/Summer20UL18/TTJets_*.npz')
-        #glob.glob(DATADIR+'/train_bkg/Summer20UL18/QCD_*.npz')
+        #glob.glob(DATADIR+'/train_bkg/Summer20UL18/TTJets_*.npz')
+        glob.glob(DATADIR+'/train_bkg/Summer20UL18/QCD_*.npz')
         #+ glob.glob(DATADIR+'/train_bkg/Summer20UL18/TTJets_*.npz')
         ]
 
